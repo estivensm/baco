ffa42d0af5a1cd8d9bca9b4692f02f30c9abec57
Carlos Gochez <cgotcha@gmail.com>
2015-04-23 16:09:33 -0600

    add client controller

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,10 +1,33 @@
 class ClientsController < ApplicationController
+  before_action :get_client, only: [:show, :edit, :destroy]
+
   def index
+    @clients = Client.order(:name)
+  end
+
+  def show
   end
 
   def new
+    @client = Client.new
+  end
+
+  def create
+
   end
 
   def edit
   end
+
+  def update
+  end
+
+  def destroy
+    @client.destroy
+  end
+
+  private
+    def get_client
+      @client = Client.find(params[:id])
+    end
 end
