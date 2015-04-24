0923a057486937cec5705b58c3f446676825d0b9
Carlos Gochez <cgotcha@gmail.com>
2015-04-23 23:54:36 -0600

    finalizando controlador de clients

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,5 +1,5 @@
 class ClientsController < ApplicationController
-  before_action :get_client, only: [:show, :edit, :destroy]
+  before_action :get_client, only: [:show, :edit, :destroy, :update]
 
   def index
     @clients = Client.order(:name)
@@ -13,21 +13,37 @@ class ClientsController < ApplicationController
   end
 
   def create
+    @client = Client.new(client_params)
 
+    if @client.save
+      redirect_to @client
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @client.update(client_params)
+      redirect_to @client
+    else
+      render :edit
+    end
   end
 
   def destroy
     @client.destroy
+    redirect_to clients_path
   end
 
   private
     def get_client
       @client = Client.find(params[:id])
+    end
+
+    def client_params
+      params.require(:client).permit(:name, :age)
     end
 end
