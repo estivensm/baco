34c3bedf7ce4a2c8c6d03123e68aef2f75f3cfc2
Carlos Gochez <cgotcha@gmail.com>
2015-04-29 18:05:43 -0600

    crear las vistas de bank account y crear vistas parciales

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   resources :clients
+  resources :bank_accounts
 
   # Example resource route with options:
   #   resources :products do
