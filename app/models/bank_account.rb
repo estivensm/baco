21ccd10fe39dce3ab1493bbbcdcb950d5ffd51fb
Carlos Gochez <cgotcha@gmail.com>
2015-08-29 20:47:36 -0600

    8-5 Usar Highcharts

--- app/models/bank_account.rb
+++ app/models/bank_account.rb
@@ -28,7 +28,10 @@ class BankAccount < ActiveRecord::Base
   def self.generar_intereses
     collection = all
     collection.each do |account|
-      account.balance = account.balance + account.balance * 0.05
+      amount = account.balance * 0.05
+      account.balance = account.balance + amount
+      #Generar una transaccion
+      account.transactions.create(label: "Interes", amount: amount)
       account.save
     end
   end
